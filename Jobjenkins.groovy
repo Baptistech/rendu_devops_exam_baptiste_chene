42816a3bcdbee5d26fcf48a8840d4f535d8dc6ad
Baptiste <baptistechene@orange.fr>
2019-02-25 15:46:56 +0100

    Job jenkins update

--- Jobjenkins.groovy
+++ Jobjenkins.groovy
@@ -1,6 +1,5 @@
 freeStyleJob('devops_master') {
     logRotator(-1, 10)
-    jdk('Java 6')
     scm {
         github('Baptistech/rendu_devops_exam_baptiste_chene', 'master')
     }
@@ -11,7 +10,31 @@ freeStyleJob('devops_master') {
       shell('docker-compose build')
       shell('docker-compose up')
     }
-    publishers {
-        archiveArtifacts('job-dsl-plugin/build/libs/job-dsl.hpi')
+}
+
+job('build_publishV1') {
+    steps {
+        dockerBuildAndPublish {
+            repositoryName('https://github.com/Baptistech/rendu_devops_exam_baptiste_chene')
+            tag('${BUILD_TIMESTAMP}-${GIT_REVISION,length=7}')
+            registryCredentials('docker-hub')
+            forcePull(false)
+            createFingerprints(false)
+            skipDecorate()
+        }
+    }
+}
+
+freeStyleJob('devops_master') {
+    logRotator(-1, 10)
+    scm {
+        github('Baptistech/rendu_devops_exam_baptiste_chene', 'master')
+    }
+    triggers {
+        githubPush()
+    }
+    steps {
+      shell('cd samplephpwebsite')
+      shell('docker build -t appPhp .')
     }
 }
