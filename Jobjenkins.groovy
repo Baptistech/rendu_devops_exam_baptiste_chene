48073925a1b15190501414fa97b79a20edab6771
Baptiste <baptistechene@orange.fr>
2019-02-25 12:11:13 +0100

    create new steps

--- Jobjenkins.groovy
+++ Jobjenkins.groovy
@@ -2,13 +2,14 @@ freeStyleJob('devops_master') {
     logRotator(-1, 10)
     jdk('Java 6')
     scm {
-        github('https://github.com/Baptistech/rendu_devops_exam_baptiste_chene', 'master')
+        github('Baptistech/rendu_devops_exam_baptiste_chene', 'master')
     }
     triggers {
         githubPush()
     }
     steps {
-        gradle('clean build')
+      shell('docker-compose build')
+      shell('docker-compose up')
     }
     publishers {
         archiveArtifacts('job-dsl-plugin/build/libs/job-dsl.hpi')
